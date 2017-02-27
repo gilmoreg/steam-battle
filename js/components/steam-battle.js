eeddcccc4545dbaf209333d3706c1dd96aa176c1
gilmoreg@live.com <gilmoreg@live.com>
2017-02-27 17:29:22 -0600

    Hello world, and test

---
+++ js/components/steam-battle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class SteamBattle extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className='steam-battle'>Hello world!</div>
+        )
+    }
+}
--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
 require('babel-polyfill');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SteamBattle from './components/steam-battle';
 
+document.addEventListener('DOMContentLoaded', () =>
+    ReactDOM.render(
+        <SteamBattle />,
+        document.getElementById('app')
+    )
+);
