63018339bbdb74a132a53bac0c9901470b7c5c36
gilmoreg@live.com <gilmoreg@live.com>
2017-03-04 01:40:09 -0600

    eslint cleanup

--- js/components/player.js
+++ js/components/player.js
@@ -3,29 +3,34 @@ import { connect } from 'react-redux';
 import Score from './Score';
 
 export class Player extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
   render() {
     const { player } = this.props;
     if (player.profile) {
-      return ( 
-          <div id={player.profile.steamid} className="player col-6 blue"> 
-            <h2><a href={player.profile.profileurl} title={player.profile.personaname} target="_blank">{player.profile.personaname}</a></h2>
-            <p>
-              <img src={player.profile.avatarfull} alt={player.profile.personaname} title={player.profile.personaname} />
-            </p>
-            <Score score={this.props.player.score} />
-          </div>
+      return (
+        <div id={player.profile.steamid} className="player col-6 blue">
+          <h2>
+            <a href={player.profile.profileurl} title={player.profile.personaname}>
+              {player.profile.personaname}
+            </a>
+          </h2>
+          <p>
+            <img
+              src={player.profile.avatarfull}
+              alt={player.profile.personaname}
+              title={player.profile.personaname}
+            />
+          </p>
+          <Score score={this.props.player.score} />
+        </div>
       );
     }
-    return ( <div></div> );
+    return (<div />);
   }
 }
 
 const mapStateToProps = (state, props) => ({
-  player: state.players[props.pid]
-})
+  player: state.players[props.pid],
+});
 
 export default connect(mapStateToProps)(Player);
