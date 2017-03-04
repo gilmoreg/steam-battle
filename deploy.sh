420823a3d6d3e3e30c220d38bc28149f3bd3679c
gilmoreg@live.com <gilmoreg@live.com>
2017-03-04 02:27:20 -0600

    test deploy script fix

--- deploy.sh
+++ deploy.sh
@@ -29,7 +29,8 @@ git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
-git add ../build
+cp -r ../build build
+git build
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
