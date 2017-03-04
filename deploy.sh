5ab0acd34a73a5d8abe3eec7ec61d897e6cc9454
gilmoreg@live.com <gilmoreg@live.com>
2017-03-04 02:21:44 -0600

    test deploy script fix

--- deploy.sh
+++ deploy.sh
@@ -30,7 +30,7 @@ git config user.email "$COMMIT_AUTHOR_EMAIL"
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 pwd
-ls
+ls ../
 git add -A
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
