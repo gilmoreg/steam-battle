65010fe17205abacf0df1f496501ca8ed8e83f58
gilmoreg@live.com <gilmoreg@live.com>
2017-03-04 16:50:40 -0600

    Remove unused ajax call

--- js/index.js
+++ js/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import axios from 'axios';
 import store from './store';
 import Root from './components/Root';
-
-// Wake up the Steam Battle API
-axios('https://protected-dusk-95868.herokuapp.com');
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
