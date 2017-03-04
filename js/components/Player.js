10d4434aaec89b89901b7e65c7a58978b54d32bf
gilmoreg@live.com <gilmoreg@live.com>
2017-03-04 10:59:38 -0600

    Refactor Player component; cleanup

--- js/components/Player.js
+++ js/components/Player.js
@@ -2,23 +2,34 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Score from './Score';
 
-export class Player extends React.Component {
-
-  render() {
-    const { player } = this.props;
-    if (player.profile) {
-      return (
-        <div id={`player${this.props.pid}`} className="player col-6 blue">
-          <Score score={this.props.player.score} />
-        </div>
-      );
-    }
-    return (<div />);
+export function Player(props) {
+  const { player } = props;
+  if (player.profile) {
+    return (
+      <div id={`player${props.pid}`} className="player col-6 blue">
+        <Score score={props.player.score} />
+      </div>
+    );
   }
+  return (<div />);
 }
 
 const mapStateToProps = (state, props) => ({
   player: state.players[props.pid],
 });
+
+Player.propTypes = {
+  pid: React.PropTypes.number.isRequired,
+  player: React.PropTypes.shape({
+    id: React.PropTypes.number.isRequired,
+    score: React.shape({
+      owned: React.PropTypes.number.isRequired,
+      playtime: React.PropTypes.number.isRequired,
+      recent: React.PropTypes.number.isRequired,
+      achievements: React.PropTypes.number.isRequired,
+      total: React.PropTypes.number.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default connect(mapStateToProps)(Player);
