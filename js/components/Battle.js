de721af0eef3869662778a565c984cfa3b5752e5
gilmoreg@live.com <gilmoreg@live.com>
2017-03-04 19:50:42 -0600

    add retry button

--- js/components/Battle.js
+++ js/components/Battle.js
@@ -2,10 +2,20 @@ import React from 'react';
 import Player from './Player';
 
 export default function Battle() {
+  const retry = (e) => {
+    e.preventDefault();
+    this.context.router.transitionTo('/');
+  };
+
   return (
     <div>
       <Player pid={0} />
       <Player pid={1} />
+      <button onClick={retry}>Battle again!</button>
     </div>
   );
 }
+
+Battle.contextTypes = {
+  router: React.PropTypes.object,
+};
