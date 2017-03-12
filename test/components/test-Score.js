9d73029546b87c0d5562a71251dd9c2c7f6a4658
Grayson Gilmore <gilmoreg@live.com>
2017-03-12 14:44:46 -0500

    Convert test to enzyme

--- test/components/test-Score.js
+++ test/components/test-Score.js
@@ -3,54 +3,32 @@
 /* eslint-disable comma-dangle */
 /* eslint-disable no-undef */
 import React from 'react';
-import TestUtils from 'react-addons-test-utils';
+import { shallow } from 'enzyme';
 import chai from 'chai';
 import Score from '../../js/components/Score';
 
 const should = chai.should();
 
 describe('Score component', () => {
+  const score = {
+    total: 0,
+    owned: 0,
+    playtime: 0,
+    recent: 0,
+  };
+
   it('should render a component with props', () => {
-    const renderer = TestUtils.createRenderer();
-    const score = {
-      total: 0,
-      owned: 0,
-      playtime: 0,
-      recent: 0,
-    };
-    renderer.render(<Score score={score} />);
-    const result = renderer.getRenderOutput();
-    result.type.should.equal('div');
-    result.props.className.should.equal('player-score');
-    // TODO What else should I test?
+    const wrapper = shallow(<Score score={score} />);
+    wrapper.node.type.should.equal('div');
+    wrapper.node.props.className.should.equal('player-score');
+    wrapper.node.props.children.type.should.equal('ul');
+    wrapper.node.props.children.props.className.should.equal('score-list');
   });
   it('should render a component with default props', () => {
-    const renderer = TestUtils.createRenderer();
-    renderer.render(<Score />);
-    const result = renderer.getRenderOutput();
-    result.type.should.equal('div');
-    result.props.className.should.equal('player-score');
-    result.props.children.type.should.equal('ul');
+    const wrapper = shallow(<Score />);
+    wrapper.node.type.should.equal('div');
+    wrapper.node.props.className.should.equal('player-score');
+    wrapper.node.props.children.type.should.equal('ul');
+    wrapper.node.props.children.props.className.should.equal('score-list');
   });
 });
-
-/*
-
-no props { '$$typeof': Symbol(react.element),
-  type: 'div',
-  key: null,
-  ref: null,
-  props:
-   { className: 'player-score',
-     children:
-      { '$$typeof': Symbol(react.element),
-        type: 'ul',
-        key: null,
-        ref: null,
-        props: [Object],
-        _owner: null,
-        _store: {} } },
-  _owner: null,
-  _store: {} }
-
-*/
